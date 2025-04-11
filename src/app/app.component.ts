bf97628f7c5bcecc9b36ab7e97099aea4605420d
Programador Full Stack 36133232024.3 <36133232024.3@senacrjedu.local>
2025-04-11 18:58:52 -0300

    atualizando o footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './componentes/top-bar/top-bar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { FaleComponent } from './componentes/fale/fale.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent,FooterComponent],
+  imports: [RouterOutlet, TopBarComponent,FooterComponent, FaleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
