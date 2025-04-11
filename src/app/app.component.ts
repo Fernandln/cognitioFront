cd7411508353a09bd57c9ea7cf2e24c06602aee5
Fernandln <fernandosousa4k@gmail.com>
2025-04-11 14:57:29 -0300

    footer e corpo da pagina fale conosco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './componentes/top-bar/top-bar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { SobreComponent } from './componentes/sobre/sobre.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent,FooterComponent],
+  imports: [RouterOutlet, TopBarComponent,FooterComponent, SobreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
