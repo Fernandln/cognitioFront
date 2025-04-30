db087f958e69c509384973406d8ea88d90b96edf
Fernandln <fernandosousa4k@gmail.com>
2025-04-30 13:43:49 -0300

    subindo login e cadastrese

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './componentes/top-bar/top-bar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { FaleconoscoComponent } from './componentes/faleconosco/faleconosco.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent,FooterComponent, FaleconoscoComponent],
+  imports: [RouterOutlet, TopBarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FaleconoscoComponent } from './componentes/faleconosco/faleconosco.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CadastrarComponent } from './componentes/cadastrar/cadastrar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: FaleconoscoComponent},
+    //{path: 'faleconosco', component: FaleconoscoComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'Cadastrar', component: CadastrarComponent}
+
+];
