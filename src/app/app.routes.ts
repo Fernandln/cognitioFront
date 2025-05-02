67e68628b5749dd88aea7f36949fef7eaa47d597
Fernandln <fernandosousa4k@gmail.com>
2025-05-02 19:48:12 -0300

    services e httpclient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { FaleconoscoComponent } from './componentes/faleconosco/faleconosco.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { CadastrarComponent } from './componentes/cadastrar/cadastrar.component';
+import { LobbyComponent } from './componentes/lobby/lobby.component';
 
 export const routes: Routes = [
     {path: '', component: FaleconoscoComponent},
     //{path: 'faleconosco', component: FaleconoscoComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'Cadastrar', component: CadastrarComponent}
+    {path: 'Cadastrar', component: CadastrarComponent},
+    {path: 'lobby', component: LobbyComponent}
 
 ];
