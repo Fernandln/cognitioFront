d6df1e933ad2fbea186ac8f8f715c05557b4c1dd
Marcos <marcostuliogonzaga14031985@gmail.com>
2025-05-06 13:49:20 -0300

    IndexFront

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { FaleconoscoComponent } from './componentes/faleconosco/faleconosco.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CadastrarComponent } from './componentes/cadastrar/cadastrar.component';
+import { IndexComponent } from './componentes/index/index.component';
+
+export const routes: Routes = [
+    {path: '', component: IndexComponent},
+    //{path: 'faleconosco', component: FaleconoscoComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'Cadastrar', component: CadastrarComponent}
+
+];
