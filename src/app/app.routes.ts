8899fc150a8bc30fc8b3f56edae36edb7b338344
Marcos <marcostuliogonzaga14031985@gmail.com>
2025-05-07 17:35:47 -0300

    Implementacao Pagina Sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { FaleconoscoComponent } from './componentes/faleconosco/faleconosco.comp
 import { LoginComponent } from './componentes/login/login.component';
 import { CadastrarComponent } from './componentes/cadastrar/cadastrar.component';
 import { IndexComponent } from './componentes/index/index.component';
+import { SobreComponent } from './componentes/sobre/sobre.component';
 
 export const routes: Routes = [
     {path: '', component: IndexComponent},
-    //{path: 'faleconosco', component: FaleconoscoComponent},
+    // {path: 'faleconosco', component: FaleconoscoComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'Cadastrar', component: CadastrarComponent}
+    {path: 'Cadastrar', component: CadastrarComponent},
+    {path: 'Sobre', component: SobreComponent}
 
 ];
